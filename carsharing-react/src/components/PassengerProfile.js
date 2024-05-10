df608a3a032f0d47dd5aca682c8942cac84d9b3a
jovanmanchev <jovanmanchev3003@gmail.com>
2024-05-10 19:18:11 +0200

    added passenger component

---
+++ carsharing-react/src/components/PassengerProfile.js
@@ -0,0 +1,76 @@
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+import '../styles/card.css'
+const Profile = ({ passengerId }) => {
+    const [passenger, setPassenger] = useState(null);
+
+    const [bio, setBio] = useState("");
+
+    const handleBioChange = (event) => {
+        setBio(event.target.value);
+    };
+
+    const fetchPassenger = async () => {
+        const hostname = process.env.REACT_APP_API_HOST || window.location.hostname;
+        try {
+            
+            const response = await axios.get(`http://${hostname}:8080/api/passengers/passenger/${passengerId}`);
+            setBio(response.data.bio)
+            setPassenger(response.data);
+        } catch (error) {
+            console.error('Failed to fetch data', error);
+        }
+    };
+
+    const handleSubmit = async (event) => {
+        event.preventDefault();
+        const data = {
+            bio: bio
+        };
+
+        try {
+            const response = await axios.post(`http://localhost:8080/api/passengers/update/${passengerId}`, data);
+            console.log('Success:', response.data);
+            alert('Passenger updated successfully!');
+        } catch (error) {
+            console.error('Error updating passenger:', error.response ? error.response.data : error.message);
+            alert('Failed to update passenger: ' + (error.response ? error.response.data.message : error.message));
+        }
+    };
+
+    useEffect(() => {
+        fetchPassenger();
+    }, [passengerId]);
+
+    if (!passenger) {
+        return <div>Loading...</div>;
+    }
+    return (
+        
+            <div className="card">
+                
+                    <div className='header'>
+                        <h5 className="title">{passenger.firstName} {passenger.lastName}</h5>
+                        
+                    </div>
+                    <div className='card-content'>
+                        <div >
+                            <div className="rating-text">Rating: {passenger.rating || 'None'} / 5</div>
+                           
+                            <div className='rating-text'>Bio</div>
+                            <form onSubmit={handleSubmit}>
+                                <input placeholder='Write something...' className='text-field mt-3' value={bio} onChange={handleBioChange}/>
+                                <button type="submit" className="submit-button">Save</button>
+
+                            </form>
+                          
+                        </div>                    
+                    </div>
+             
+               
+            </div>
+        
+    );
+};
+
+export default Profile;
